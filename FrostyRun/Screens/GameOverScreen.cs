3032546b594cade26939f7fac552ee439663a3f1
Marnik Maes <marnikmaes46@hotmail.com>
2024-12-03 11:21:09 +0100

    GameOverScreen

---
+++ FrostyRun/Screens/GameOverScreen.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrostyRun.Screens
+{
+    public class GameOverScreen
+    {
+        public GameOverScreen() 
+        {
+            // This is where you can call Debug.WriteLine
+            Debug.WriteLine("Has to be coded");
+        }
+    }
+}
